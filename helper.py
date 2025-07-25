7619cb62bdfeaffd5df6d8e21b99e3a4b8a1edb3
Jimena Hernández <her21199@uvg.edu.gt>
2025-07-25 09:53:43 -0600

    Reapply "Add placeholder comments to helper.py"
    This reverts commit fbac50f98786b0be51c88ca92d0e8afd15b35e2e.

--- helper.py
+++ helper.py
@@ -138,3 +138,5 @@ def execute_graph_agent_code(code: str, table_data: list, output_file:str = "out
     # print(f"[DEBUG] - Imagen subida correctamente: {public_url}")
     return public_url
 
+### 
+### MOD 
